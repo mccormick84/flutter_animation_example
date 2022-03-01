476e6b1ed4761005e223d2c6d40fb6fd3cfe8eb9
Kenny <mccormick84@gmail.com>
2022-03-01 22:05:53 +0900

    그래프 애니메이션 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animation_example/people.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,20 +19,108 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class AnimationApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _AnimationApp();
 }
 
-
 class _AnimationApp extends State<AnimationApp> {
+  List<People> peoples = List.empty(growable: true);
+  int current = 0;
+
+  @override
+  void initState() {
+    peoples.add(People('스미스', 180, 92));
+    peoples.add(People('메리', 160, 55));
+    peoples.add(People('존', 177, 75));
+    peoples.add(People('바트', 130, 40));
+    peoples.add(People('콘', 194, 140));
+    peoples.add(People('디디', 100, 80));
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Animation Example'),
       ),
-      body: Container(),
+      body: Container(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                child: Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 100,
+                      child: Text('이름: ${peoples[current].name}'),
+                    ),
+                    AnimatedContainer(
+                      duration: const Duration(seconds: 1),
+                      curve: Curves.bounceIn,
+                      color: Colors.amber,
+                      child: Text(
+                        '키 ${peoples[current].height}',
+                        textAlign: TextAlign.center,
+                      ),
+                      width: 50,
+                      height: peoples[current].height,
+                    ),
+                    AnimatedContainer(
+                      duration: const Duration(seconds: 1),
+                      curve: Curves.easeInCubic,
+                      color: Colors.blue,
+                      child: Text(
+                        '몸무게 ${peoples[current].weight}',
+                        textAlign: TextAlign.center,
+                      ),
+                      width: 50,
+                      height: peoples[current].weight,
+                    ),
+                    AnimatedContainer(
+                      duration: const Duration(seconds: 1),
+                      curve: Curves.linear,
+                      color: Colors.pinkAccent,
+                      child: Text(
+                        'bmi ${peoples[current].bmi.toString().substring(0, 2)}',
+                        textAlign: TextAlign.center,
+                      ),
+                      width: 50,
+                      height: peoples[current].bmi,
+                    ),
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                ),
+                height: 200,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    if (current < peoples.length - 1) {
+                      current++;
+                    }
+                  });
+                },
+                child: const Text('다음'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    if (current > 0) {
+                      current--;
+                    }
+                  });
+                },
+                child: const Text('이전'),
+              )
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+        ),
+      ),
     );
   }
 }
