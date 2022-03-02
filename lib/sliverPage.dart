a2b780ae0a9d2f13f3b90f9638ca24ad82b846a9
Kenny <mccormick84@gmail.com>
2022-03-02 18:27:40 +0900

    위젯 구분 머리말 추가

--- lib/sliverPage.dart
+++ lib/sliverPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class SliverPage extends StatefulWidget {
   const SliverPage({Key? key}) : super(key: key);
@@ -22,14 +23,57 @@ class _SliverPageState extends State<SliverPage> {
               background: Image.asset('repo/images/sunny.png'),
             ),
             backgroundColor: Colors.deepOrangeAccent,
+            // 스크롤 시 앱바 사라짐 여부
+            pinned: true,
+          ),
+          SliverPersistentHeader(
+            delegate: _HeaderDelegate(
+              minHeight: 50,
+              maxHeight: 150,
+              child: Container(
+                color: Colors.blue,
+                child: Center(
+                  child: Column(
+                    children: const <Widget>[
+                      Text(
+                        'list 숫자',
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                ),
+              ),
+            ),
+            pinned: true,
           ),
           SliverList(
-              delegate: SliverChildListDelegate([
-            customCard('1'),
-            customCard('2'),
-            customCard('3'),
-            customCard('4'),
-          ])),
+              delegate: SliverChildBuilderDelegate((context, index) {
+            return Container(
+              child: customCard('list count: $index'),
+            );
+          }, childCount: 10)),
+          SliverPersistentHeader(
+            delegate: _HeaderDelegate(
+              minHeight: 50,
+              maxHeight: 150,
+              child: Container(
+                color: Colors.blue,
+                child: Center(
+                  child: Column(
+                    children: const <Widget>[
+                      Text(
+                        '그리드 숫자',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                ),
+              ),
+            ),
+            pinned: true,
+          ),
           SliverGrid(
               delegate: SliverChildListDelegate([
                 customCard('1'),
@@ -51,10 +95,44 @@ class _SliverPageState extends State<SliverPage> {
         child: Center(
           child: Text(
             text,
-            style: const TextStyle(fontSize: 40),
+            style: const TextStyle(fontSize: 30),
           ),
         ),
       ),
     );
+  }
+}
+
+class _HeaderDelegate extends SliverPersistentHeaderDelegate {
+  final double minHeight;
+  final double maxHeight;
+  final Widget child;
+
+  _HeaderDelegate({
+    required this.minHeight,
+    required this.maxHeight,
+    required this.child,
+  });
+
+  // 머리말을 만들 때 사용할 위젯을 배치
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return SizedBox.expand(child: child);
+  }
+
+  // math.max 함수를 이용해 가장 높은 값을 최대 높이로 설정
+  @override
+  double get maxExtent => math.max(maxHeight, minHeight);
+
+  @override
+  double get minExtent => minHeight;
+
+  @override
+  // 위젯을 새로 그릴지 여부
+  bool shouldRebuild(_HeaderDelegate oldDelegate) {
+    return maxHeight != oldDelegate.maxHeight ||
+        minHeight != oldDelegate.minHeight ||
+        child != oldDelegate.child;
   }
 }
